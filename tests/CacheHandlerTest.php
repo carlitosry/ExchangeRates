94918d25d24bb62e01e10ad0293c06d09727bffd
Carlos Reyes <reyes.syscom@gmail.com>
2023-04-01 23:25:49 +0200

    TESTS: Fixed Test dependencies

---
+++ tests/CacheHandlerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Tests;
+
+use App\Service\Cache\CacheHandler;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class CacheHandlerTest extends TestCase
+{
+    public function testSetItem()
+    {
+        // Create a mock CacheInterface
+        $cache = $this->createMock(RedisAdapter::class);
+        $cacheItem = $this->createMock(ItemInterface::class);
+
+
+        // Set up expectations for the mock CacheInterface
+        $cache->expects($this->atLeastOnce())
+            ->method('getItem')
+            ->with('foo')
+            ->willReturn($cacheItem);
+
+        $cache->expects($this->once())
+            ->method('save')
+            ->with($cacheItem);
+
+        // Create a new instance of CacheHandler using the mock CacheInterface
+        $cacheHandler = new CacheHandler($cache);
+
+        // Call setItem on the CacheHandler
+        $cacheHandler->setItem('foo', 'bar');
+
+    }
+
+    public function testGetItem()
+    {
+        // Create a mock CacheInterface
+        $cache = $this->createMock(RedisAdapter::class);
+        $cacheItem = $this->createMock(ItemInterface::class);
+
+        $cacheItem->expects($this->once())
+            ->method('get')
+            ->willReturn('bar');
+
+        $cacheItem->expects($this->once())
+            ->method('isHit')
+            ->willReturn(true);
+
+        // Set up expectations for the mock CacheInterface
+        $cache->expects($this->atLeastOnce())
+            ->method('getItem')
+            ->with('foo')
+            ->willReturn($cacheItem);
+
+        // Create a new instance of CacheHandler using the mock CacheInterface
+        $cacheHandler = new CacheHandler($cache);
+
+        // Call getItem on the CacheHandler to get the value we just set
+        $result = $cacheHandler->getItem('foo');
+
+        // Check that the result is equal to the value we set
+        $this->assertEquals('bar', $result);
+
+    }
+}
---
+++ tests/RatesProviderTest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Tests;
+
+use App\Repository\ExchangeRateRepository;
+use App\Service\Handler\CacheHandler;
+use App\Service\Provider\RatesProvider;
+use App\Model\RatesResponse;
+use App\Service\Provider\SerializerProvider;
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class RatesProviderTest extends TestCase
+{
+    private MockObject $client;
+    private MockObject $serializerProvider;
+    private MockObject $serializer;
+    private MockObject $cacheHandler;
+    private MockObject $exchangeRateRepository;
+
+    protected function setUp(): void
+    {
+        $this->client = $this->createMock(Client::class);
+        $this->cacheHandler = $this->createMock(CacheHandler::class);
+        $this->serializerProvider = $this->createMock(SerializerProvider::class);
+        $this->exchangeRateRepository = $this->createMock(ExchangeRateRepository::class);
+        $this->serializer = $this->getMockBuilder(SerializerInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testGetRatesSuccess()
+    {
+        $jsonResponse = '{"success": true, "base": "USD", "rates": {"EUR": 0.825, "GBP": 0.708, "JPY": 108.78}}';
+
+        $this->serializerProvider->expects($this->once())
+            ->method('create')
+            ->willReturn(
+                $this->serializer
+            );
+
+        $this->serializer->expects($this->once())
+            ->method('deserialize')
+            ->with($jsonResponse, RatesResponse::class, 'json')
+            ->willReturn(
+                $this->getFakeRateResponse("USD", ['EUR' => 0.825, 'GBP' => 0.708, 'JPY' => 108.78])
+            );
+
+        $this->client->expects($this->once())
+            ->method('request')
+            ->willReturn(
+                new Response(200, [], $jsonResponse)
+            );
+
+        $ratesProvider = new RatesProvider(
+            $this->client,
+            $this->serializerProvider,
+            $this->cacheHandler,
+            $this->exchangeRateRepository
+        );
+
+        $ratesResponse = $ratesProvider->getRates('USD', ['EUR', 'GBP', 'JPY']);
+
+        $this->assertInstanceOf(RatesResponse::class, $ratesResponse);
+        $this->assertTrue($ratesResponse->getSuccess());
+        $this->assertEquals('USD', $ratesResponse->getBase());
+        $this->assertEquals(['EUR' => 0.825, 'GBP' => 0.708, 'JPY' => 108.78], $ratesResponse->getRates());
+    }
+
+
+    public function testGetRatesFail()
+    {
+        $this->client->expects($this->once())
+            ->method('request')
+            ->willReturn(new Response(400, [], '{"error": "Bad Request"}'));
+
+        $ratesProvider = new RatesProvider(
+            $this->client,
+            $this->serializerProvider,
+            $this->cacheHandler,
+            $this->exchangeRateRepository
+        );
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('The response status code is not valid');
+
+        $ratesProvider->getRates('USD', ['EUR', 'GBP', 'JPY']);
+    }
+
+    private function getFakeRateResponse($base = 'EUR', $ratesArray = ['USD' => 0.99]): RatesResponse
+    {
+        $rates = new RatesResponse();
+        $rates->setBase($base);
+        $rates->setRates($ratesArray);
+        $rates->setSuccess(true);
+
+        return $rates;
+    }
+}
